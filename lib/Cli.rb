3fb7abb825d7b809e6231faa4950e5fe89bf9959
Kyle Trahan <ktrahan2@gmail.com>
2020-08-18 17:04:48 -0400

    updates to function

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -1,5 +1,26 @@
 class Cli 
 
+    attr_accessor :dimensions, :function, :price
+
+    def initialize dimensions = nil, function = nil, price = nil
+        @dimensions = nil
+        @function = nil
+        @price = nil
+    end
+    
+    # def store front
+    # tty prompt
+        #get a computer recommendation
+        #returning customers - wishlist
+        # refer to a friend
+        #if get a compiuter recommendation
+            #store_introduction 
+        #elsif
+            # returning customers
+
+
+
+
     def store_introduction 
         puts Ascii.store_name 
         puts "Khajiits has computers if you have answers!"
@@ -14,8 +35,6 @@ class Cli
     end
 
     def computer_questions
-        @dimensions = nil
-        @function = nil
         puts Ascii.store_name #change later to khajiit random ascii
         puts "Are you looking for a laptop or desktop?"
         @dimensions = gets.chomp
@@ -26,14 +45,14 @@ class Cli
         puts "Fantastic Khajiit loves #{@function}."
         puts Ascii.store_name #change later to khajiit random ascii
         puts "Finally, how much are you looking to spend?"
-        price = gets.chomp
+        @price = gets.chomp
         puts Ascii.store_name #change later to khajiit random ascii
         puts "Khajiit knows just the one!"
         computer_selection = Computer.where(dimensions: @dimensions, function: @function) #add price whenever figure out how to.
         binding.pry
         puts Ascii.store_name #change later
         puts "Khajiit has listened and chosen:"
-        puts computer_selection.computers.map { }
+        puts computer_selection.computers.map
         # Recommendation.new at the end of this process
         # make.models.pluck(:name).map { |name| puts name}
     end
