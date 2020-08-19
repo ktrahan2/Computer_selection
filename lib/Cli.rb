5b894201eb2cf3183076ba59a0a749179bc63637
vamigoya97 <amigo.victor.97@gmail.com>
2020-08-19 11:39:38 -0400

    prompt

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -1,5 +1,22 @@
 class Cli 
 
+    def store_front
+
+        prompt.select("Choose one option") do |menu|
+            # menu.cycle true  # so that selection cycles around when reach top or bottom
+            menu.choice "Find Computer", store_introduction
+            menu.choice "Wishlist", 2 #wishlist
+            menu.choice "Refer to a friend", 3 # desired value
+        end
+    end
+
+    # def brands
+    #     prompt.multi_select("Choose the brands you like: ", brands)
+    #     # brands can be an unique array of all the brands in our database 
+    #     # .multi_select will always return an array by default populated
+    #     # with the names of choices. Can return custom values. 
+    # end
+
     def store_introduction 
         puts Ascii.store_name 
         puts "Khajiits has computers if you have answers!"
@@ -13,11 +30,11 @@ class Cli
         puts "Welcome #{name}, Khajiit is here to serve."
     end
 
-    def first_question
-        puts Ascii.store_name
-        puts "Are you looking for a laptop or desktop?"
-        dimension = gets.chomp
-        puts "Awesome we have plenty of #{dimension}s"
-    end
+    # def first_question
+    #     puts Ascii.store_name
+    #     puts "Are you looking for a laptop or desktop?"
+    #     dimension = gets.chomp
+    #     puts "Awesome we have plenty of #{dimension}s"
+    # end
         
 end
--- runner.rb
+++ runner.rb
@@ -2,8 +2,10 @@ require_relative 'config/environment'
 
 app = Cli.new
 
+app.store_front
 
 # binding.pry
-app.store_introduction
-app.first_question
-binding.pry
+# app.store_introduction
+# app.first_question
+
+#binding.pry
