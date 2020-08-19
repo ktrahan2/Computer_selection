18fe3827915ce342dee443a1bd8b8caa0574860f
Kyle Trahan <ktrahan2@gmail.com>
2020-08-19 15:50:36 -0400

    fixed runner.rb

--- runner.rb
+++ runner.rb
@@ -1,17 +1,8 @@
 require_relative 'config/environment'
 
 app = Cli.new
-<<<<<<< HEAD
-# binding.pry
-# app.store_introduction
-# app.computer_selection
+
 app.store_front
 binding.pry
 
 
-=======
-binding.pry
-app.store_front
-binding.pry
-
->>>>>>> 27af0379d207027f18212fa88ad88272c9b7e8e0
